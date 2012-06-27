e7c6923e3618ed161b086245ac2aa4d721a2016b
vagrant <vagrant@precise32.(none)>
2012-06-27 19:23:03 +0400

    GInitial Commit. enerated the Rails 3 application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ticketee::Application.config.secret_token = 'bea3bef202bc8bf1793abe180764e026082057f47351837ac8f8885ba6d7e397c3b24143ee1bc8a4144a39056e0aa3b1b3c5c669a64a1a9641e093def89fdcaa'
